011a55a6470fad221d8422dc22d913a5049ce885
Buse Berren <72972981+busolbuse@users.noreply.github.com>
2023-09-16 17:33:24 +0300

    Create Ornek_4.php

---
+++ Ornek_4.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="tr-TR">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="todo_list.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <title>Ornek-4</title>
+    <style>
+    body{
+        background-color: black;
+        color: white;
+    }
+</style>
+</head>
+<body>
+<?php 
+    echo "Buse Berren ÜNAL - PHP örnek 3 - Tırnak sorunu- Türkiye'nin bla bla - Buurada slash  a gerek kalamdan tek ve çift tırnak kullanarak sorunu çözebiliriz.";
+?> 
+</body>
+</html>
