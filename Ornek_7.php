9489059aa2e43303b3801e26691f39016ae248d9
Buse Berren <72972981+busolbuse@users.noreply.github.com>
2023-09-16 17:48:28 +0300

    Create Ornek_7.php

---
+++ Ornek_7.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="tr-TR">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="todo_list.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <title>Ornek-6</title>
+    <style>
+    body{
+        background-color: black;
+        color: white;
+    }
+</style>
+</head>
+<body>
+<?php 
+    echo "böyle de ", "yazıla", "bilir";
+    echo 'Buse Berren ÜNAL - PHP örnek 7 - echo nun farklı kullanım şekilleri';
+    echo ("böyle de yazılabilir");
+    
+?> 
+</body>
+</html>
