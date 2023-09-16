ce9cc5ea152d876ac9582fa4f5eefbc63c0d3ea3
Buse Berren <72972981+busolbuse@users.noreply.github.com>
2023-09-16 17:38:58 +0300

    Create Ornek_5.php

---
+++ Ornek_5.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="tr-TR">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="todo_list.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <title>Ornek-4</title>
+    <style>
+    body{
+        background-color: black;
+        color: white;
+    }
+</style>
+</head>
+<body>
+<?php 
+    $ornekBir = "BU bir ORNEKK değişkenden gelennn";
+    echo "Buse Berren ÜNAL - PHP örnek 5 - $ornekBir -değişkenin içindeki yazıyı getirdik";
+?> 
+</body>
+</html>
