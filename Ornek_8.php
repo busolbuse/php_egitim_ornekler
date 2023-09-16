116e76bd4d8b0e482b47a77849a8d677bfb56a81
Buse Berren <72972981+busolbuse@users.noreply.github.com>
2023-09-16 17:57:18 +0300

    Create Ornek_8.php

---
+++ Ornek_8.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="tr-TR">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="todo_list.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <title>Ornek-8</title>
+    <style>
+    body{
+        background-color: black;
+        color: white;
+    }
+</style>
+</head>
+<body>
+    <?php
+    $deneme = "etiketle yapılmış bi yazı"
+    ?>
+    <?php
+    echo $deneme;
+    ?>
+
+    <?=$deneme ?>
+
+   <?="kısa yolla echo yerine ekrana yazdırma." ?>
+</body>
+</html>
