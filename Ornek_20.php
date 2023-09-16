21b7c21274d5dd5106ee0195e3a5173cc6615643
Buse Berren <72972981+busolbuse@users.noreply.github.com>
2023-09-16 20:01:13 +0300

    Create Ornek_20.php

---
+++ Ornek_20.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="tr-TR">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="todo_list.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <title>OrnekLERR</title>
+    <style>
+    body{
+        background-color: black;
+        color: white;
+    }
+</style>
+</head>
+<body>
+    <form action = "sonuc.php" method="get">
+       isim: <input type="text" name="isim" ><br />
+       soyisim:  <input type="text" name="soyisim" ><br />
+       <input type="button" value="GONDER" ><br />
+    </form>
+    <?php
+ 
+    ?>
+</body>
+</html>
