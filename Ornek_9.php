7591f7a6e7e3189062389481b6e60401518705d1
Buse Berren <72972981+busolbuse@users.noreply.github.com>
2023-09-16 18:02:40 +0300

    Create Ornek_9.php

---
+++ Ornek_9.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="tr-TR">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="todo_list.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <title>OrnekLERR</title>
+    <style>
+    body{
+        background-color: black;
+        color: white;
+    }
+</style>
+</head>
+<body>
+    <?php
+    print "Buse Berren Ünal";
+    print 'Buse Türkiye\'nin en harikası! burda tırnaktan kaçma var.';
+    $bubirdegisgen = "degisgeeeeeeeeen";
+    print " $bubidegisgen  degisken cagırmaaa";
+    print <<<END
+    BLA BLA BLAAAAAAAAAAAAAAAAAAAA
+    AAAAAAAAAAA
+    AAAAA
+    END
+
+    ?>
+    
+</body>
+</html>
