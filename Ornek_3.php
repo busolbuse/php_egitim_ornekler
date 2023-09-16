8651d40b95cc90cd262d97190674c58bdab3ce9d
Buse Berren <72972981+busolbuse@users.noreply.github.com>
2023-09-16 17:23:13 +0300

    Create Ornek_3.php

---
+++ Ornek_3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="tr-TR">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="todo_list.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <title>Ornek-3</title>
+    <style>
+    body{
+        background-color: black;
+        color: white;
+    }
+</style>
+</head>
+<body>
+<?php 
+    echo 'Buse Berren ÜNAL - PHP örnek 3 - Tırnak sorunu- Türkiye\'nin bla bla - kodda yanında \' bu eklenmeli.';
+?>
+    <input type="button" onClick="Deneme();" value="Işlem Yap"><br /><br  />
+    <div id="IslemAlani"></div> 
+</body>
+</html>
