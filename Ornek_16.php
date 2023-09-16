835660cb1d31f93117ddca04d1f6e917d16abc35
Buse Berren <72972981+busolbuse@users.noreply.github.com>
2023-09-16 19:23:17 +0300

    Create Ornek_16.php

---
+++ Ornek_16.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="tr-TR">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="todo_list.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <title>OrnekLERR</title>
+    <style>
+    body{
+        background-color: black;
+        color: white;
+    }
+</style>
+</head>
+<body>
+    <?php
+    echo "değişken tekrardannnnn";
+    echo "<br />";
+
+    $isimsoyisim = "Buse Ünal";//her biri bir karakterdir.
+    echo $isimsoyisim[1];
+    echo "<br />";
+    echo $isimsoyisim[0];
+    echo $isimsoyisim[3] . "<br />";
+    echo $isimsoyisim[4] . "<br />";
+    echo $isimsoyisim[5] . "<br />";
+
+    ?>
+</body>
+</html>
