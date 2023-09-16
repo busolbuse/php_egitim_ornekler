ea181422b41c3de11528e5818ebad27bb3d74705
Buse Berren <72972981+busolbuse@users.noreply.github.com>
2023-09-16 18:06:53 +0300

    Create Ornek_10.php

---
+++ Ornek_10.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="tr-TR">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="todo_list.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <title>OrnekLERR</title>
+    <style>
+    body{
+        background-color: black;
+        color: white;
+    }
+</style>
+</head>
+<body>
+    <?php
+    //bu eiklde yorum satırı oluşturulur 
+    //sayfanın kaynak kodunda da eklenmez.
+    #de olurrr
+    /*
+        Çoklu yorum satırıı
+
+    */
+    print "yorum satırı öğreniyoz"
+
+    ?>
+    
+</body>
+</html>
