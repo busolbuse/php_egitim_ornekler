87d13f800efbf76c843f21f00eff796bc87f798f
Buse Berren <72972981+busolbuse@users.noreply.github.com>
2023-09-16 18:13:34 +0300

    Create Ornek_11.php

---
+++ Ornek_11.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="tr-TR">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="todo_list.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <title>OrnekLERR</title>
+    <style>
+    body{
+        background-color: black;
+        color: white;
+    }
+</style>
+</head>
+<body>
+    <?php
+    phpinfo(INFO_LICENSE);
+    phpinfo(INFO_CREDITS);
+    phpinfo(INFO_GENERAL);
+    phpinfo(INFO_CONFIGURATION);
+    phpinfo(INFO_ENVIRONMENT);
+    ?>
+</body>
+</html>
