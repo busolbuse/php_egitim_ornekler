2714f938aa40b911273d955e016f12dab0768aca
Buse Berren <72972981+busolbuse@users.noreply.github.com>
2023-09-17 21:12:38 +0300

    Create Ornek_34.php

---
+++ Ornek_34.php
@@ -0,0 +1,33 @@
+<?php
+namespace projelerbuse; // satır 105 ile alakalı.
+//kesinlikle üstünde hiçbir kod yazılı olmamalı aksi takdirde hata .
+?>
+<!DOCTYPE html>
+<html lang="tr-TR">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="todo_list.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <title>OrnekLERR</title>
+    <style>
+    body{
+        background-color: black;
+        color: white;
+    }
+</style>
+</head>
+<body>
+<?php
+
+//                      _Null koşul ifadesi -get ile
+
+
+ ?>   
+ <form action="Ornek_34_1.php" method="get">
+    İsim : <input type="text", name= "kullaniciismi"><br />
+    Soyİsim : <input type="text", name= "kullanicisoyismi"><br />
+    <input type="submit" value="formu gönder!">
+ </form>
+
+</body>
+</html>
