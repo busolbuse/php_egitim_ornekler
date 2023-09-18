b450977f624a77bc67481e303f52c10cbd41c4df
Buse Berren <72972981+busolbuse@users.noreply.github.com>
2023-09-18 13:39:33 +0300

    Create Ornek_34_1.php

---
+++ Ornek_34_1.php
@@ -0,0 +1,33 @@
+<?php
+namespace projelerbuse; // satır 105 ile alakalı.
+//kesinlikle üstünde hiçbir kod yazılı olmamalı aksi takdirde hata .
+?>
+<!DOCTYPE html>
+<html lang="tr-TR">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="todo_list.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <title>OrnekLERR</title>
+    <style>
+    body{
+        background-color: black;
+        color: white;
+    }
+</style>
+</head>
+<body>
+<?php
+
+//                      _Null koşul ifadesi -get ile
+
+
+    $gelenisimdegeri = $_GET["kullaniciismi"] ?? "HATA";// ?? den sonra istediğimizi ekleyebiliriz ister başka bir sayfaya yönlendirilsin 
+    //kötü amaçlı kullanıcılar input u silebiliyor bunu önlemek için bu şekilde yapılır.
+    $gelensoyisimdegeri = $_GET["kullanicisoyismi"]  ?? "HATA";// genelde başka sayfalara yönlendirilir.
+    echo "fromdan gelen isim degeri : " . $gelenisimdegeri ."<br>";
+    echo "fromdan gelen soyisim degeri : " . $gelensoyisimdegeri ."<br>";
+
+ ?>   
+</body>
+</html>
