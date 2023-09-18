5a63ff58808428f9a3ac21031a35cc04b3cc2d11
Buse Berren <72972981+busolbuse@users.noreply.github.com>
2023-09-18 13:46:50 +0300

    Create Ornek_35.php

---
+++ Ornek_35.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="tr-TR">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="todo_list.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <title>OrnekLERR</title>
+    <style>
+    body{
+        background-color: black;
+        color: white;
+    }
+</style>
+</head>
+<body>
+<?php
+
+//                      _Null koşul ifadesi -post ile
+
+
+ ?>   
+ <form action="Ornek_35_1.php" method="post">
+    İsim : <input type="text", name= "kullaniciismi"><br />
+    Soyİsim : <input type="text", name= "kullanicisoyismi"><br />
+    <input type="submit" value="formu gönder!">
+ </form>
+
+</body>
+</html>
