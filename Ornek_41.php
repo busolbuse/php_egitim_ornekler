e7f95ad95e12ec1aff30e960a5efa00b3ac45d4e
Buse Berren <72972981+busolbuse@users.noreply.github.com>
2023-09-19 13:36:15 +0300

    Create Ornek_41.php

---
+++ Ornek_41.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html lang="tr-TR">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="todo_list.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <title>OrnekLERR</title>
+    <style>
+    body{
+        background-color: black;
+        color: white;
+    }
+</style>
+</head>
+<body>
+<?php
+//---------------------------------------------------------------------
+//                               :MAT.SEL İŞLEMLER: 
+
+
+//işlemleri direkt ekrana yazdırırken de yapabiliriz.
+echo 100/3 . "<br>";// sonuç = 33.333333333333
+
+
+//kalan- modülüs - 
+echo 50 % 3.2 . "<br>";//2 
+
+
+
+
+?>
+
+
+
+
+
+
+</body>
+</html>
