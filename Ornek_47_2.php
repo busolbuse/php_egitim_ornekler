c318b2d70661b422e406adb129e2603aac98d9f8
Buse Berren <72972981+busolbuse@users.noreply.github.com>
2023-09-19 16:13:57 +0300

    Create Ornek_47_2.php

---
+++ Ornek_47_2.php
@@ -0,0 +1,38 @@
+<?php
+require_once("Ornek_47_3_ayar_dosyasi.php");// böylece çektik.
+?>
+<!DOCTYPE html>
+<html lang="tr-TR">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="todo_list.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <title>OrnekLERR</title>
+    <style>
+    body{
+        background-color: goldenrod;
+        color: white;
+    }
+</style>
+</head>
+<body>
+<?php
+
+//---------------------------------------------------------------------
+//                 :  COOKİE(ÇEREZ) ve SESSION (OTURUM) YÖNTEMLERİ:
+
+//                      :çıkış sayfası sessionları YOK Et!!!!!:
+
+session_destroy();
+
+echo "<a href='Ornek_47.php'>Forma geri dön!</a>"
+ 
+?>
+
+
+
+
+
+
+</body>
+</html>
