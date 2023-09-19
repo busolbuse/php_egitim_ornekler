ad04ba977356c31c9a07912acb4ef8edd29bdbe5
Buse Berren <72972981+busolbuse@users.noreply.github.com>
2023-09-19 15:32:54 +0300

    Create Ornek_45.php

---
+++ Ornek_45.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="tr-TR">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="todo_list.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <title>OrnekLERR</title>
+    <style>
+    body{
+        background-color: black;
+        color: white;
+    }
+</style>
+</head>
+<body>
+<?php
+
+//---------------------------------------------------------------------
+//                 :  COOKİE(ÇEREZ) ve SESSION (OTURUM) YÖNTEMLERİ:
+
+// örneğin : bir website içerisinde önce kullanıcı kullanıcı bir sayfaye geçmek iiçin tıkladığında...
+// ... onu o sayfada kendi ismiyle hitap edelim 
+//bunun için cookie ve session kullanılır. 
+
+// örneğin 2 : üye giriş işlemleri içinde böyle yapılır.
+?>
+    <form action="Ornek_45_1.php" method="post">
+            ADINIZ: <br><input type="text" name="Adi"><br />
+            SOYADINIZ: <br> <input type="text" name="Soyadi"><br />
+            <input type="submit" value="Gönder">
+    </form>
+
+
+
+
+
+</body>
+</html>
