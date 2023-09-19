de02873ffbb479f39a3db664ee4f931604ea39b8
Buse Berren <72972981+busolbuse@users.noreply.github.com>
2023-09-19 16:13:03 +0300

    Create Ornek_47.php

---
+++ Ornek_47.php
@@ -0,0 +1,38 @@
+<?php
+require_once("Ornek_47_3_ayar_dosyasi.php");// böylece çektik.
+?>
+
+<!DOCTYPE html>
+<html lang="tr-TR">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="todo_list.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <title>OrnekLERR</title>
+    <style>
+    body{
+        background-color: black;
+        color: white;
+    }
+</style>
+</head>
+<body>
+<?php
+
+//---------------------------------------------------------------------
+//                 :  COOKİE(ÇEREZ) ve SESSION (OTURUM) YÖNTEMLERİ:
+
+
+?>
+    <form action="Ornek_47_1.php" method="post">
+        KULLANICI ADI: <br><input type="text" name="Adi"><br />
+        ŞİFRE: <br> <input type="password" name="Kullanicininsifresi"><br />
+            <input type="submit" value="Giriş YAP">
+    </form>
+
+
+
+
+
+</body>
+</html>
